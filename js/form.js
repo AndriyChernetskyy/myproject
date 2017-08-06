326f437a1af3f43b623afa2d063f44e704df8041
Andriy Chernetskyy <andrewusa921@gmail.com>
2017-08-06 19:54:45 +0000

    no empty form

--- js/form.js
+++ js/form.js
@@ -34,6 +34,17 @@
 });
 
 */
+document.getElementById('submit').addEventListener('click', sendForm);
+
+function sendForm(e) {
+	e.preventDefault();
+    const form = document.getElementById('form');
+    if (form.checkValidity()) {
+    	alert('Form is OK')    
+    } else {
+    	alert('Form is not OK');
+    }   
+}
 
 $(function() {
     $('form input[type=submit]').click(sendForm);
