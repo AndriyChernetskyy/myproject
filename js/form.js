825b354529aaa0bfd289bb22101a4849c0c8c6b5
Andriy Chernetskyy <andrewusa921@gmail.com>
2017-08-07 10:24:35 +0000

    preresult

--- js/form.js
+++ js/form.js
@@ -34,6 +34,17 @@
 });
 
 */
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10&appId=842739302568340";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+
+
 document.getElementById('submit').addEventListener('click', sendForm);
 
 function sendForm(e) {
